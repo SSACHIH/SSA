e03db210ec62b47fc2419db6f12cc700f5c5438a
joelsaldana <gwkogure@hotmail.com>
2015-09-10 13:51:31 -0600

    Clase Example Revert a creacion
    Nos regresamos a la clase vacia

--- src/com/example/example.java
+++ src/com/example/example.java
@@ -1,13 +1,10 @@
 package com.example;
-import java.util.*;
 
 public class example {
 
 	public static void main(String[] args) {
-		System.out.println("hola mundo");
-for (int i = 0; i < args.length; i++) {
-	
-}
+		// TODO Auto-generated method stub
+
 	}
 
 }
